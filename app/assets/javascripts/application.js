c82ab18feb41947ce62b171e66885278eddf19e0
Rails Udemy Course <railsudemycourse@gmail.com>
2018-12-11 13:59:51 +0200

    Completed app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,4 +23,7 @@ $(document).on('turbolinks:load', function(){
 
 $(document).ready(function() {
     $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').transition('fade');
+  });
 });
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Evgeny", password: "password")
+User.create(username: "Jonsnow", password: "password")
+User.create(username: "Arya", password: "password")
+User.create(username: "Frodo", password: "password")
+User.create(username: "Gandalf", password: "password")
+
+Message.create(body: "This is a new message", user_id: 1)
+Message.create(body: "This is a second message", user_id: 2)
+Message.create(body: "This is a third message", user_id: 3)
+Message.create(body: "This is a forth message", user_id: 4)
